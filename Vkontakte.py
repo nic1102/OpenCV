10fe8e5759b62362aa1fe68bfee253015b5b8c35
nic1102 <nic110245@gmail.com>
2023-12-26 20:20:06 +0300

    Рефакторинг

--- Vkontakte.py
+++ Vkontakte.py
@@ -3,6 +3,10 @@ import vk_api.vk_api
 from Social import Social
 from secret import *
 
+"""
+https://vkhost.github.io/
+"""
+
 
 class Vkontakte(Social):
     def __init__(self, token=access_token):
@@ -11,14 +15,14 @@ class Vkontakte(Social):
     def send_post(self):
         self.vk.method("wall.post", {
             'v': '5.199',
-            'access_token': access_token,
-            'owner_id': -1,
+            'owner_id': -222103827,
             'message': 'message',
             'friends_only': 0,
-            'from_group': 1
+            'from_group': 1,
+            'attachments': "https://sun9-24.userapi.com/impf/DlL6XoI28NM6mhof0qfzHk-ZuZOYJM0-h-FUTA/0WJWM4D3iZc.jpg?size=500x500&quality=96&sign=38987bce630c408a1238a737a7d1ae31&type=album"
         })
 
-    def send_msg(self, user_id, text):
+    def send_msg(self, user_id: int, text: str):
         self.vk.method("messages.send", {
             "user_id": user_id,
             "message": text,
