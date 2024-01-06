85fa57a32d6e12cef868d4fce46201e57488c45a
nic1102 <nic110245@gmail.com>
2024-01-06 22:13:24 +0300

    Рефакторинг

--- YaDisk.py
+++ YaDisk.py
@@ -7,12 +7,22 @@ class YaDisk:
         self.disk = yadisk.YaDisk(token=yandex_token)
 
     def save_images(self, images: list, is_raw: bool):
-        if self.disk.check_token(): #Проверка токена
+        if self.disk.check_token():
             ...
         else:
             return NotImplemented
+
+    def get_dir_info(self, dir_name='/Test_Dir/1'):
+        for item in self.disk.listdir(dir_name):
+            print(f"Название: {item['name']}")
+            print(f'Размер: {item["size"]} байт')
+            print(f"Тип файла: {item['type']}")
+            print(f"Тип документа: {item['media_type']}")
+            print(f"Дата создания: {item['created']}\n")
+
     def check_raw_images(self):
-        ...
+        NotImplemented
+
 
 y = YaDisk()
-print(list(y.disk.listdir("/Test_Dir")))
+y.get_dir_info()
